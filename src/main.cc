b60a13979018ca542e6d7f4335497a7cd0cd110a
bhud23 <ebennetthudson@gmail.com>
2022-11-29 12:28:06 -0500

    building observer for Bhavya

---
+++ src/main.cc
@@ -0,0 +1,18 @@
+/*
+    main entry point for program, holds main game loop
+*/
+#include "subject.h"
+#include "piece.h"
+#include "gamemanager.h"
+#include "blank.h"
+#include "pawn.h"
+#include "bishop.h"
+#include "rook.h"
+#include "knight.h"
+#include "queen.h"
+#include "king.h"
+
+int main (int arc, char **argv) {
+    
+    return 0;
+}
